f72da1da733210bb53334569684074681d3e6211
Ahmad495 <amdagha01@gmail.com>
2021-09-10 15:38:30 +0500

    Made a delete middleware that deletes the reviewRef.

--- models/campGround.js
+++ models/campGround.js
@@ -1,5 +1,6 @@
 const { string, number } = require('joi');
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -28,6 +29,14 @@ const CampgroundSchema = new Schema({
             type: Schema.Types.ObjectId, ref: 'Review'
         }
     ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: { $in: doc.reviewsRef }
+        })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
