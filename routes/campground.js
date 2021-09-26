109f575b163354dafade3c5ca22fd2aa743b1a75
Ahmad495 <amdagha01@gmail.com>
2021-09-26 12:16:48 +0500

    uploading file from form.

--- routes/campground.js
+++ routes/campground.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
-
 const catchAsync = require('../utils/catchAsync');
-
 const Campground = require('../models/campGround');
-
 const { isLoggedIn, validationCampground, isAuthorCampground } = require('../Middleware');
-
 const campgroundsController = require('../controllers/campgroundsController');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 
 router.get('/', catchAsync(campgroundsController.index));
 
-router.get('/new', isLoggedIn, campgroundsController.newCampground)
+router.get('/new', isLoggedIn, campgroundsController.newCampground);
 
-router.post('/', isLoggedIn, validationCampground, catchAsync(campgroundsController.createNewCampground))
+// router.post('/', isLoggedIn, validationCampground, catchAsync(campgroundsController.createNewCampground));
 
-router.put('/:id', isLoggedIn, isAuthorCampground, validationCampground, catchAsync(campgroundsController.editCampground))
+router.post('/', upload.array('campground[image]'), (req, res) => {
+    console.log(req.body, req.files);
+})
 
-router.delete('/:id', isLoggedIn, isAuthorCampground, catchAsync(campgroundsController.deleteCampground))
+router.put('/:id', isLoggedIn, isAuthorCampground, validationCampground, catchAsync(campgroundsController.editCampground));
 
-router.get('/:id/edit', isLoggedIn, isAuthorCampground, catchAsync(campgroundsController.showCampgroundEidt))
+router.delete('/:id', isLoggedIn, isAuthorCampground, catchAsync(campgroundsController.deleteCampground));
+
+router.get('/:id/edit', isLoggedIn, isAuthorCampground, catchAsync(campgroundsController.showCampgroundEidt));
 
 router.get('/:id', catchAsync(campgroundsController.showCampground));
 
