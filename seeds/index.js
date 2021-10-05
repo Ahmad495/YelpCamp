11aaf0c7feab550157b3f6f3be16262a4948c0a0
Ahmad495 <amdagha01@gmail.com>
2021-10-05 20:48:05 +0500

    mongodb atlas seeding

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDb = async () => {
     await Campground.deleteMany({});
-    for (let i = 1; i <= 550; i++) {
+    for (let i = 1; i <= 100; i++) {
         const price = Math.floor(Math.random() * 1000) + 1;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
