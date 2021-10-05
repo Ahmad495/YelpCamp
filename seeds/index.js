d66ecabd9b9f1f24ae5885897f28488a98b5be7c
Ahmad495 <amdagha01@gmail.com>
2021-10-05 20:49:39 +0500

    mongodb atlas seeding

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { descriptors, places } = require('./seedHelpers');
 const Campground = require('../models/campGround');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
